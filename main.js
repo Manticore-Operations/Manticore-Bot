99bf61cbf12cf1d8f24cf397933b8c138e98186e
ryker-dev <pizzuti.dev@gmail.com>
2023-02-10 17:12:15 +0200

    Rewrote main to new standards

--- main.js
+++ main.js
@@ -1,66 +1,45 @@
-const { Client, Intents, Collection } = require('discord.js');
-const client = new Client({
-	intents: [Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
-	partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
-});
-// Node filesystem
-const fs = require('fs');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { token, clientId } = require('./config.json');
 
-// Import config
-const { token } = require('./config.json');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// ////////////////////
-
-// -- READ SLASH COMMANDS
-
-client.commands = new Collection();
-const commandFiles = fs
-	.readdirSync('./commands')
-	.filter((file) => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	// Set a new item in the Collection
-	// With the key as the command name and the value as the exported module
-	client.commands.set(command.data.name, command);
-}
 
 const deploy = require('./deploy-commands.js');
-deploy();
+deploy;
 
-// -- READ EVENTS
+client.commands = new Collection();
 
-const eventFiles = fs
-	.readdirSync('./events')
-	.filter((file) => file.endsWith('.js'));
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-for (const file of eventFiles) {
-	const event = require(`./events/${file}`);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	}
-	else {
-		client.on(event.name, (...args) => event.execute(...args));
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 	}
 }
 
-// ---------------------------------
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
 
-client.on('interactionCreate', async (interaction) => {
-	if (!interaction.isCommand()) return;
+	const command = interaction.client.commands.get(interaction.commandName);
 
-	const command = client.commands.get(interaction.commandName);
-	if (!command) return;
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
 
 	try {
-		await command.execute(interaction, client);
-	}
-	catch (error) {
+		await command.execute(interaction);
+	} catch (error) {
 		console.error(error);
-		await interaction.reply({
-			content: 'There was an error while executing this command!',
-			ephemeral: true,
-		});
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
 
