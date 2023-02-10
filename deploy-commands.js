d837e8e3f877f1dded27a8c0271fc262e0472715
ryker-dev <pizzuti.dev@gmail.com>
2023-02-10 17:12:03 +0200

    Rewrite deploy-commands to new standard

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,40 +1,34 @@
-/* Used to deploy global commands to all guilds */
+const { REST, Routes } = require('discord.js');
+const fs = require('node:fs');
+const { token, clientId, guildId } = require('./config.json');
 
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const { appId, guildId, token } = require('./config.json');
-const fs = require('fs');
+const commands = [];
+// Grab all the command files from the commands directory you created earlier
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-module.exports = () => {
-	const commands = [];
-	const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
 
-	for (const file of commandFiles) {
-		const command = require(`./commands/${file}`);
-		commands.push(command.data.toJSON());
+// Construct and prepare an instance of the REST module
+const rest = new REST({ version: '10' }).setToken(token);
+
+// and deploy your commands!
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
 	}
-
-	const rest = new REST({ version: '9' }).setToken(token);
-
-	(async () => {
-		try {
-			console.log('Started refreshing application (/) commands.');
-
-			await rest.put(
-				Routes.applicationGuildCommands(appId, guildId),
-				{ body: commands },
-			);
-
-			console.log('Successfully reloaded application (/) commands.');
-		}
-		catch (error) {
-			console.error(error);
-		}
-	})();
-
-/* 	const rest = new REST({ version: '9' }).setToken(token);
-	rest
-		.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-		.then(() => console.log('Successfully registered application commands.'))
-		.catch(console.error); */
-};
+})();
